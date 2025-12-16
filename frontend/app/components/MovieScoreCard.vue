7be6c52f96b3812a2ddbc7f738a9c67aca28a8ed
rtix <28888263+rtix@users.noreply.github.com>
2025-12-17 00:36:19 +0300

    style: enhance MovieScoreCard visuals

--- frontend/app/components/MovieScoreCard.vue
+++ frontend/app/components/MovieScoreCard.vue
@@ -1,15 +1,22 @@
 <template>
-  <div>
+  <div class="movie-score-card">
     <NuxtImg
+      class="movie-score-card__poster"
       :src="
         movie.posterPath &&
         `${useRuntimeConfig().public.tmdbImageBase}/original${movie.posterPath}`
       "
       placeholder="/images/placeholder.jpg"
-      style="height: 5rem; display: inline"
     />
-
-    {{ movie.title }} {{ likes }}
+    <div class="movie-score-card__info">
+      <div class="movie-score-card__title">
+        {{ movie.title }}
+        <span v-if="likes" class="movie-score-card__likes">
+          {{ likes }} <Icon name="material-symbols:favorite" />
+        </span>
+      </div>
+      <p>{{ movie.overview }}</p>
+    </div>
   </div>
 </template>
 
@@ -18,4 +25,29 @@ import type { MovieDto } from "~~/shared/types/generated/MovieDto";
 defineProps<{ movie: MovieDto; likes?: number }>();
 </script>
 
-<style scoped></style>
+<style scoped>
+.movie-score-card {
+  display: flex;
+  align-items: center;
+
+  padding-block: var(--spacing-xs);
+}
+
+.movie-score-card__poster {
+  width: var(--spacing-3xl);
+}
+
+.movie-score-card__info {
+  padding: var(--spacing-sm);
+}
+
+.movie-score-card__title {
+  font-weight: bold;
+}
+
+.movie-score-card__likes {
+  display: inline-flex;
+  align-items: center;
+  color: var(--color-secondary);
+}
+</style>
